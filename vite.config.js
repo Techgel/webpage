a5083ca3512d0652bcdb073b14dc1bfecb664098
Jose Angel Vergara <josevergara66166@gmail.com>
2025-12-17 02:52:23 -0600

    Configuración final para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/webpage/",   // <--- ¡AGREGA ESTO! (Es el nombre de tu repositorio)
 })
